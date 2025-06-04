1ea1fc40994ce656c60f2cf2ed7e25fb74fb55bb
Pushpendra160 <95026765+Pushpendra160@users.noreply.github.com>
2025-06-04 11:28:12 +0530

    Update constants.js

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const Constants = {
   // BASE_URL: 'https://kartalucia-backend.vercel.app/api',
   
-BASE_URL: "https://test-back-psi.vercel.app/api",
+BASE_URL: "https://test-backend-xi-pink.vercel.app/api",
 getTokens: () => ({
   adminloggedIn: localStorage.getItem("adminToken"),
 }),
