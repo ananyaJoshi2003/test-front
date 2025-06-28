979ce8c0db24dc0850f359e38e8e7dd62522d83c
ananyaJoshi2003 <77530072+ananyaJoshi2003@users.noreply.github.com>
2025-06-28 14:27:14 +0530

    Update constants.js

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const Constants = {
   // BASE_URL: 'https://kartalucia-backend.vercel.app/api',
   
-BASE_URL: "https://test-back-ashy.vercel.app/api",
+BASE_URL: "https://test-back-ashy.vercel.app/",
 getTokens: () => ({
   adminloggedIn: localStorage.getItem("adminToken"),
 }),
